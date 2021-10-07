0728a2fa550bcce5ea70e2f5dfdd9ed1b96490f4
Jin Lou <jinlou@bu.edu>
2021-10-07 14:05:25 -0400

    Changes from the 10/1 lab

--- 02-library/cs506/read.py
+++ 02-library/cs506/read.py
@@ -1,6 +1,14 @@
+import csv
+
 def read_csv(csv_file_path):
     """
         Given a path to a csv file, return a matrix (list of lists)
         in row major.
     """
-    raise NotImplementedError()
+    two_dim_list = []  # snake case ftw (PEP8)
+    with open(csv_file_path, 'r') as f:
+        r = csv.reader(f, delimiter=',')
+        # next(r)  # skip header line if necessary
+        for row in r:
+            two_dim_list.append(row)
+    return two_dim_list
--- 02-library/cs506/sim.py
+++ 02-library/cs506/sim.py
@@ -1,3 +1,6 @@
+from math import sqrt
+# import numpy as np
+
 def euclidean_dist(x, y):
     res = 0
     for i in range(len(x)):
@@ -5,12 +8,20 @@ def euclidean_dist(x, y):
     return res**(1/2)
 
 def manhattan_dist(x, y):
-    raise NotImplementedError()
+    return sum(abs(val1-val2) for val1, val2 in zip(x,y))
 
 def jaccard_dist(x, y):
-    raise NotImplementedError()
+    # num of observations in both sets
+    both = len(list(set(x).intersection(y)))
+    # num of observations in either set
+    either = (len(x) + len(y)) - both
+    return float(both) / either
 
 def cosine_sim(x, y):
+    # a1=np.array(x)
+    # a2=np.array(y)
+    # sim_scores = a1.dot(a2)/ (np.linalg.norm(a1, axis=1) * np.linalg.norm(a2))
+    # return sim_scores
     raise NotImplementedError()
 
 # Feel free to add more
